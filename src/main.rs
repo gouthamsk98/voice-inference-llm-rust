e0ce0d7d9a09b627959a2186418d85f906c6a987
gouthamsk98 <gouthamben10@gmail.com>
2024-10-26 13:25:33 +0000

    update_l2:simple linear layer added

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,26 @@
 use candle_core::{ Device, Result, Tensor };
 
+struct Linear {
+    weight: Tensor,
+    bias: Tensor,
+}
+
+impl Linear {
+    fn forward(&self, x: &Tensor) -> Result<Tensor> {
+        let x = x.matmul(&self.weight)?;
+        x.broadcast_add(&self.bias)
+    }
+}
 struct Model {
-    first: Tensor,
-    second: Tensor,
+    first: Linear,
+    second: Linear,
 }
 
 impl Model {
     fn forward(&self, image: &Tensor) -> Result<Tensor> {
-        let x = image.matmul(&self.first)?;
+        let x = self.first.forward(image)?;
         let x = x.relu()?;
-        x.matmul(&self.second)
+        self.second.forward(&x)
     }
 }
 
@@ -17,8 +28,17 @@ fn main() -> Result<()> {
     // Use Device::new_cuda(0)?; to use the GPU.
     let device = Device::new_cuda(0)?;
 
-    let first = Tensor::randn(0f32, 1.0, (784, 100), &device)?;
-    let second = Tensor::randn(0f32, 1.0, (100, 10), &device)?;
+    //creating a dummy model
+
+    let weight = Tensor::randn(0f32, 1.0, (784, 100), &device)?;
+    let bias = Tensor::randn(0f32, 1.0, (100,), &device)?;
+
+    let first = Linear { weight, bias };
+
+    let weight = Tensor::randn(0f32, 1.0, (100, 10), &device)?;
+    let bias = Tensor::randn(0f32, 1.0, (10,), &device)?;
+
+    let second = Linear { weight, bias };
     let model = Model { first, second };
 
     let dummy_image = Tensor::randn(0f32, 1.0, (1, 784), &device)?;
