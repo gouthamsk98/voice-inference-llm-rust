d588c4542213247b797fbe5991fcc97273fd280d
gouthamsk98 <gouthamben10@gmail.com>
2024-11-01 22:34:51 +0530

    Update:load a model

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
-use candle_core::{ Device, Result, Tensor };
-use candle_nn::{Linear, Module};
-
-struct Model {
-    first: Linear,
-    second: Linear,
-}
-
-impl Model {
-    fn forward(&self, image: &Tensor) -> Result<Tensor> {
-        let x = self.first.forward(image)?;
-        let x = x.relu()?;
-        self.second.forward(&x)
-    }
-}
+use candle_core::{ Device, Result, Tensor, DType };
+use candle_nn::{ Linear, Module };
+use hf_hub::api::sync::Api;
 
 fn main() -> Result<()> {
     // Use Device::new_cuda(0)?; to use the GPU.
@@ -20,21 +8,17 @@ fn main() -> Result<()> {
 
     //creating a dummy model
 
-     // This has changed (784, 100) -> (100, 784) !
-    let weight = Tensor::randn(0f32, 1.0, (100, 784), &device)?;
-    let bias = Tensor::randn(0f32, 1.0, (100,), &device)?;
-    //stored as (output_dim, input_dim).
-    let first = Linear::new(weight, Some(bias));
+    let api = Api::new().unwrap();
+    let repo = api.model("bert-base-uncased".to_string());
 
-    let weight = Tensor::randn(0f32, 1.0, (10, 100), &device)?;
-    let bias = Tensor::randn(0f32, 1.0, (10,), &device)?;
+    let weights_filename = repo.get("model.safetensors").unwrap();
 
-    let second = Linear::new(weight, Some(bias));
-    let model = Model { first, second };
-
-    let dummy_image = Tensor::randn(0f32, 1.0, (1, 784), &device)?;
-
-    let digit = model.forward(&dummy_image)?;
-    println!("Digit {digit:?} digit");
+    let weights = candle_core::safetensors::load(weights_filename, &device)?;
+    let weight = weights.get("bert.encoder.layer.0.attention.self.query.weight").unwrap();
+    let bias = weights.get("bert.encoder.layer.0.attention.self.query.bias").unwrap();
+    let linear = Linear::new(weight.clone(), Some(bias.clone()));
+    let input_ids = Tensor::zeros((3, 768), DType::F32, &device).unwrap();
+    let output = linear.forward(&input_ids).unwrap();
+    println!("Output {output:?} output");
     Ok(())
 }
